e435332dca0496dccfa9cebd5724443531c71a6f
jsanchezramirez <joseangel-66@hotmail.es>
2020-07-16 18:35:38 +0200

    progreso4

--- src/main/java/com/joseangel/prueba/services/CalcServiceImpl.java
+++ src/main/java/com/joseangel/prueba/services/CalcServiceImpl.java
@@ -1,16 +1,21 @@
 package com.joseangel.prueba.services;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalcServiceImpl implements CalcService {
 
 	@Override
-	public Double suma(String ... valores) {
+	public Double suma(String ... valores) throws NumberFormatException {
 		
-		System.err.println(valores);
+		List<Double> nums=Arrays.asList(valores).stream().map(op -> 
+			op==null?0.0:Double.parseDouble(op)).collect(Collectors.toList());
 		
-		return null;
+		return nums.stream().reduce(0.0, (a,b) -> a+b);
 	}
 
 }
