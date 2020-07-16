ba0a5a45ee5ae40a5e0e50ec824a0173527d5180
jsanchezramirez <joseangel-66@hotmail.es>
2020-07-16 14:27:10 +0200

    progreso2

--- src/main/java/com/joseangel/prueba/controllers/CalculatorController.java
+++ src/main/java/com/joseangel/prueba/controllers/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.joseangel.prueba.controllers;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,12 @@ public class CalculatorController {
 
 	@GetMapping("/inValores/{val1}/{val2}")
 	public Double inValores(@PathVariable String val1, @PathVariable String val2) {
+		System.out.println(val1+"---"+val2);
+		return 2.12345;
+	}
+	
+	@GetMapping("/suma")
+	public Double reqSuma(@PathParam(value = "val1") String val1, @PathParam(value = "val2") String val2) {
 		System.out.println(val1+"---"+val2);
 		return 2.12345;
 	}
